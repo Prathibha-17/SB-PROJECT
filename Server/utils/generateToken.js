a94581b13881c90ebf639308b57e828d0923b4c6
Bunny <gunasai111@gmail.com>
2026-07-05 12:58:31 +0530

    Delete Server directory

--- Server/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET || 'bookstore_super_secret_jwt_key_2026', {
-    expiresIn: process.env.JWT_EXPIRE || '30d',
-  });
-};
-
-export default generateToken;
